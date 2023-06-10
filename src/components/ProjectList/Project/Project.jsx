16b7303b64664753c1391e18758e4b9f129fbf2d
kobyrthr-dev <kobyrthr@gmail.com>
2023-06-09 22:53:16 -0400

    mapping project card

--- src/components/ProjectList/Project/Project.jsx
+++ src/components/ProjectList/Project/Project.jsx
@@ -2,39 +2,15 @@ import React from 'react'
 import cardContainer from "./Project.modules.css"
 
 
-const Project = () => {
-    const projects = [
-        {
-            id:1,
-            title:'Savvy Marketplace',
-            description:'Lorem ipsum dolor sit amet consectetur.'
-        },
-        {
-            id:2,
-            title:'Public Art Walk',
-            description:'Lorem ipsum dolor sit amet consectetur.'
-        },
-        {
-            id:1,
-            title:'Savvy Marketplace',
-            description:'Lorem ipsum dolor sit amet consectetur.'
-        },
-        {
-            id:2,
-            title:'Public Art Walk',
-            description:'Lorem ipsum dolor sit amet consectetur.'
-        }
-    ]
+const Project = ({id,title,description}) => {   
   return (
     
-    <div className='cardContainer'>
-          {projects.map((project,i)=>(
-            <div key ={i}>
-                <div className="styles.project-image"></div>
-                <div className="styles.project-details">Lorem ipsum dolor sit amet consectetur.</div>
+
+            <div key ={id} className='cardContainer'>
+                <div className="styles.project-image">{title}</div>
+                <div className="styles.project-details">{description}</div>
             </div>
-            ))}
-    </div>
+
   )
 }
 
--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import Project from './Project/Project'
 const ProjectList = () => {
+    const projects = [
+        {
+            id:1,
+            title:'Savvy Marketplace',
+            description:'Lorem ipsum dolor sit amet consectetur.'
+        },
+        {
+            id:2,
+            title:'Public Art Walk',
+            description:'Lorem ipsum dolor sit amet consectetur.'
+        },
+        {
+            id:1,
+            title:'Savvy Marketplace',
+            description:'Lorem ipsum dolor sit amet consectetur.'
+        },
+        {
+            id:2,
+            title:'Public Art Walk',
+            description:'Lorem ipsum dolor sit amet consectetur.'
+        }
+    ]
   return (
     <section>
-      <Project></Project>
+      {projects.map((project,i)=>(
+        <Project{...project} key={i}></Project>
+        ))}
     </section>
   )
 }
