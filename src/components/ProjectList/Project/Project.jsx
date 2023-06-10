38b9674921c3dcbe36ac3449492402ebd2dcdfd1
kobyrthr-dev <kobyrthr@gmail.com>
2023-06-09 23:23:14 -0400

    added project image

--- src/components/ProjectList/Project/Project.jsx
+++ src/components/ProjectList/Project/Project.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import cardContainer from "./Project.modules.css"
+import thumbnail from '../../../images/savvy-thumbnail.png'
 
 
 const Project = ({id,title,description}) => {   
   return (
-    
-
             <div key ={id} className='cardContainer'>
-                <div className="styles.project-image">{title}</div>
-                <div className="styles.project-details">{description}</div>
+                <img src={thumbnail}className="styles.projectImage"></img>
+                <div className="styles.projectDetails">
+                {title}<br></br>{description}</div>
             </div>
 
   )
